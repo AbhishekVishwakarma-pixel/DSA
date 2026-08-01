ea0fee169604876e5f7f1cf570081702dff2f8e8
AbhishekVishwakarma-pixel <abhishekvishwakarma200567@gmail.com>
2026-08-01 21:14:49 +0530

    Time: 1 ms (97.52%) | Memory: 47.9 MB (12.29%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -7,6 +7,7 @@ class Solution {
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(matrix[i][j]==0){
+                    // we are marking one becoz we have not decalred any value when we decalre any an array without giving value it intially store zero !!
                     a[i]=1;
                     b[j]=1;
                 }
