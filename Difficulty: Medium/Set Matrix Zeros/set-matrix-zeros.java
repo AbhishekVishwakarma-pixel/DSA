75322c18a9c7c469f36e3cc360f38c484ae9fd5d
AbhishekVishwakarma-pixel <abhishekvishwakarma200567@gmail.com>
2026-08-01 21:46:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Set Matrix Zeros/set-matrix-zeros.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void setMatrixZeroes(int[][] matrix) {
+        // code here
+        int m=matrix.length;
+        int n=matrix[0].length;
+        int[] a=new int[m];
+        int[] b=new int[n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==0){
+                    // we are marking one becoz we have not decalred any value when we decalre any an array without giving value it intially store zero !!
+                    a[i]=1;
+                    b[j]=1;
+                }
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(a[i]==1 || b[j]==1){
+                   matrix[i][j]=0;
+                }
+            }
+        }
+    }
+}
