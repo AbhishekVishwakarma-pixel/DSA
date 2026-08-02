a1167278bcd63be51d5de280c439ff0904bcddf4
AbhishekVishwakarma-pixel <abhishekvishwakarma200567@gmail.com>
2026-08-02 12:10:33 +0530

    Time: 1 ms (97.41%) | Memory: 47.4 MB (92.21%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -2,24 +2,49 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int m=matrix.length;
         int n=matrix[0].length;
-        int[] a=new int[m];
-        int[] b=new int[n];
+        int x=1;
+        int y=1;
+
+        for(int j=0;j<n;j++){
+            if(matrix[0][j]==0){
+                x=0;
+            }
+        }
         for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+            if(matrix[i][0]==0) y=0;
+        }
+
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
                 if(matrix[i][j]==0){
-                    // we are marking one becoz we have not decalred any value when we decalre any an array without giving value it intially store zero !!
-                    a[i]=1;
-                    b[j]=1;
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
                 }
             }
         }
-        for(int i=0;i<m;i++){
+        for(int j=1;j<n;j++){
+            if(matrix[0][j]==0){
+                for(int i=0;i<m;i++){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        for(int i=1;i<m;i++){
+            if(matrix[i][0]==0){
+                for(int j=0;j<n;j++){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        if(x==0){
             for(int j=0;j<n;j++){
-                if(a[i]==1 || b[j]==1){
-                   matrix[i][j]=0;
-                }
+                matrix[0][j]=0;
             }
         }
-        
+        if(y==0){
+            for(int i=0;i<m;i++){
+                matrix[i][0]=0;
+            }
+        }
     }
 }
