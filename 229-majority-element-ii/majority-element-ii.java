fab3ad928c5badb79013e8ba73e8e55e41ccd63e
AbhishekVishwakarma-pixel <abhishekvishwakarma200567@gmail.com>
2026-08-07 18:57:33 +0530

    Time: 3 ms (72.43%) | Memory: 52.6 MB (53.56%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,26 +1,36 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-
-        List<Integer> ans = new ArrayList<>();
-
-        for (int i = 0; i < nums.length; i++) {
-
-            // Avoid adding the same element multiple times
-            if (ans.contains(nums[i])) continue;
-
-            int cnt = 0;
-
-            for (int j = 0; j < nums.length; j++) {
-                if (nums[i] == nums[j]) {
-                    cnt++;
-                }
+        int el1=0;
+        int el2=0;
+        int cnt1=0;
+        int cnt2=0;
+        for(int i=0;i<nums.length;i++){
+            if(cnt1==0 && nums[i]!=el2){
+                cnt1=1;
+                el1=nums[i];
+            }
+            else if(cnt2==0 && nums[i]!=el1){
+                cnt2=1;
+                el2=nums[i];
+            }
+            else if(nums[i]==el1) cnt1++;
+            else if(nums[i]==el2) cnt2++;
+            else{
+                cnt1--;
+                cnt2--;
             }
 
-            if (cnt > nums.length / 3) {
-                ans.add(nums[i]);
-            }
         }
+        int cnt3=0;
+        int cnt4=0;
+        List<Integer> list=new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==el1) cnt3++;
+            else if(nums[i]==el2) cnt4++;
+        }
+        if(cnt3>(nums.length)/3) list.add(el1);
+        if(cnt4>(nums.length)/3) list.add(el2);
 
-        return ans;
+        return list;
     }
 }
