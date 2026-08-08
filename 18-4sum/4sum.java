af99f91f1180de4ad08c02cceb57de8baea05096
AbhishekVishwakarma-pixel <abhishekvishwakarma200567@gmail.com>
2026-08-08 19:21:29 +0530

    Time: 476 ms (5.06%) | Memory: 47.7 MB (5.72%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,41 +1,27 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
+        Set<List<Integer>> set=new HashSet<>();
+        int n=nums.length;
 
-        Set<List<Integer>> set = new HashSet<>();
-        int n = nums.length;
+        for(int i=0;i<n-2;i++){
+            for(int j=i+1;j<n-1;j++){
+                Set<Long> nset=new HashSet<>();
+                for(int k=j+1;k<n;k++){
+                   long l=(long)target-nums[i]-nums[j]-nums[k];
+                   if(nset.contains(l)){
+                    List<Integer> temp=new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[j]);
+                    temp.add(nums[k]);
+                    temp.add((int)l);
+                    Collections.sort(temp);
+                    set.add(temp);
+                   }
+                   nset.add((long)nums[k]);
 
-        for (int i = 0; i < n - 3; i++) {
-
-            for (int j = i + 1; j < n - 2; j++) {
-
-                HashSet<Long> nset = new HashSet<>();
-
-                for (int k = j + 1; k < n; k++) {
-
-                    long l = (long) target
-                           - nums[i]
-                           - nums[j]
-                           - nums[k];
-
-                    if (nset.contains(l)) {
-
-                        List<Integer> temp = new ArrayList<>();
-
-                        temp.add(nums[i]);
-                        temp.add(nums[j]);
-                        temp.add(nums[k]);
-                        temp.add((int) l);
-
-                        Collections.sort(temp);
-
-                        set.add(temp);
-                    }
-
-                    nset.add((long) nums[k]);
                 }
             }
         }
-
         return new ArrayList<>(set);
     }
 }
