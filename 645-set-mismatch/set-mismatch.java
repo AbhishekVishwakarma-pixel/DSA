b98c2921e31bf4bf19b69d9348358b86f161eae0
AbhishekVishwakarma-pixel <abhishekvishwakarma200567@gmail.com>
2026-08-11 20:29:17 +0530

    Time: 694 ms (6.16%) | Memory: 47.4 MB (77.36%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,19 +1,19 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-       Map<Integer,Integer> map=new HashMap<>();
-       int n=nums.length;
-       for(int i=1;i<=n;i++){
-        map.put(i,0);
-       } 
-       for(int i=0;i<n;i++){
-        map.put(nums[i],map.get(nums[i
-        ])+1);
-       }
-       int[] arr=new int[2];
-       for(int i=1;i<=n;i++){
-        if(map.get(i)==2) arr[0]=i;
-        if(map.get(i)==0) arr[1]=i;
-       }
-       return arr;
+        int n=nums.length;
+        int r=-1,m=-1;
+        for(int i=1;i<=n;i++){
+            int cnt=0;
+            for(int j=0;j<n;j++){
+                if(nums[j]==i){
+                    cnt++;
+                }
+            }
+            if(cnt==0) m=i;
+            else if(cnt==2) r=i; 
+            if(m!=-1 && r!=-1) break;
+        }
+        int[] arr=new int[]{r,m};
+        return arr;
     }
 }
