6dc0e26192426517e8973e909ecd060d60120814
AbhishekVishwakarma-pixel <abhishekvishwakarma200567@gmail.com>
2026-08-12 19:12:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Missing And Repeating/missing-and-repeating.java
@@ -0,0 +1,24 @@
+class Solution {
+    ArrayList<Integer> findTwoElement(int arr[]) {
+        // code here
+        int n=arr.length;
+        long S=0,S2=0;
+        long Sn=((long)n*(n+1))/2;
+        long S2n=((long)n*(n+1)*(2*n+1))/6;
+        for(int i=0;i<n;i++){
+            S+=arr[i];
+            S2+=(long)arr[i]*arr[i];
+        }
+        long val1=S-Sn;
+        long val2=S2-S2n;
+        val2=val2/val1;
+        //  x is the repeating number
+        long x=(val1+val2)/2;
+        //  y is the missing value
+        long y=x-val1;
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add((int)x);
+        list.add((int)y);
+        return list;
+    }
+}
