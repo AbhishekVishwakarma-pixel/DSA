9d7179edcb7358a0968cfa4a9504bbedaf917ca3
AbhishekVishwakarma-pixel <abhishekvishwakarma200567@gmail.com>
2026-09-06 17:10:37 +0530

    Time: 47 ms (45.99%) | Memory: 79 MB (65.63%) - LeetSync

--- 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.java
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.java
@@ -1,5 +1,4 @@
 class Solution {
-
     boolean canWePlace(int[] arr,int dist,int cows){
         int count=1;
         int last=arr[0];
@@ -8,24 +7,21 @@ class Solution {
                 count++;
                 last=arr[i];
             }
-            if(count>=cows) return true;
+            if(count>=cows)return true;
         }
         return false;
     }
     public int maxDistance(int[] arr, int k) {
         Arrays.sort(arr);
-        int low = 1;
-        int high = arr[arr.length - 1] - arr[0];
-        int ans=0;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(canWePlace(arr,mid,k)){
-                
-                low=mid+1;
-            }
-            else high=mid-1;
+       int low=1;
+       int high=arr[arr.length-1]-arr[0]; 
+       while(low<=high){
+        int mid=low +(high-low)/2;
+        if(canWePlace(arr,mid,k)){
+            low=mid+1;
         }
-        return high
-        ;
+        else high=mid-1;
+       }
+       return high;
     }
 }
