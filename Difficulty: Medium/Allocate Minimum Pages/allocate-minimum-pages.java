1ee050c52c85bb187f8efc2db48ae42fa17eefe7
AbhishekVishwakarma-pixel <abhishekvishwakarma200567@gmail.com>
2026-09-07 20:57:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.java
+++ Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.java
@@ -1,21 +1,21 @@
 class Solution {
     boolean isValid(int[] arr,long mid,int k){
-        int allocatedSt=1;
+        int isAllocated=1;
         int pages=0;
         for(int i=0;i<arr.length;i++){
-            if(arr[i]>mid) return false;
+            if(mid<arr[i]) return false;
             if(arr[i]+pages>mid){
-                allocatedSt++;
+                isAllocated++;
                 pages=arr[i];
             }
             else pages+=arr[i];
         }
-        if(allocatedSt>k) return false;
+        if(isAllocated>k) return false;
         return true;
     }
     public int findPages(int[] arr, int k) {
         // code here
-        if(arr.length<k) return -1;
+        if(arr.length<k)return -1;
         long low=0;
         long high=0;
         for(int i:arr){
@@ -23,13 +23,13 @@ class Solution {
             high+=i;
         }
         while(low<=high){
-            long mid=low +(high-low)/2;
+            long mid=low+(high-low)/2;
             if(isValid(arr,mid,k)){
                 high=mid-1;
             }
             else low=mid+1;
         }
-        return (int) low;
-        
+        return (int)low;
+    
     }
 }
