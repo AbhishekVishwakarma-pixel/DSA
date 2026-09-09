aada6833ea590378fa79443d1ab24a41df122558
AbhishekVishwakarma-pixel <abhishekvishwakarma200567@gmail.com>
2026-09-09 20:31:06 +0530

    Time: 2 ms (46.03%) | Memory: 49 MB (32.19%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -3,34 +3,42 @@ class Solution {
         List<Integer>arr=new ArrayList<>();
         int m=nums1.length;
         int n=nums2.length;
+        int t=m+n;
         int i=0;
         int j=0;
+        int cnt=0;
+        int el1=0;
+        int el2=0;
+        int ind2=t/2;
+        int ind1=ind2-1;
         while(i<m && j<n){
             if(nums1[i]<nums2[j]){
-                arr.add(nums1[i]);
-                i++;
-            }
-            else if(nums1[i]==nums2[j]){
-                arr.add(nums1[i]);
-                arr.add(nums1[i]);
-                i++;
-                j++;
+              if(cnt==ind1) el1=nums1[i];
+              if(cnt==ind2) el2=nums1[i];
+              cnt++;
+              i++;
             }
             else{
-                arr.add(nums2[j]);
+                if(cnt==ind1) el1=nums2[j];
+                if(cnt==ind2) el2=nums2[j];
                 j++;
+                cnt++;
             }
         }
         while(i<m){
-            arr.add(nums1[i]);
+            if(cnt==ind1) el1=nums1[i];
+            if(cnt==ind2) el2=nums1[i];
             i++;
+            cnt++;
         }
         while(j<n){
-            arr.add(nums2[j]);
+            if(cnt==ind1) el1=nums2[j];
+            if(cnt==ind2) el2=nums2[j];
             j++;
+            cnt++;
         }
         int total=m+n;
-        if(total%2!=0) return arr.get(total/2);
-        else return (arr.get(total/2)+arr.get(total/2 -1))/2.0;
+        if(total%2!=0) return el2;
+        else return (el1+el2)/2.0;
     }
 }
