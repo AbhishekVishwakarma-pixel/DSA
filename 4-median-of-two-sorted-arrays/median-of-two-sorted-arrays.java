a81888b1cfef4338b427c101669ba67a69e5ffff
AbhishekVishwakarma-pixel <abhishekvishwakarma200567@gmail.com>
2026-09-09 19:58:46 +0530

    Time: 2 ms (46.03%) | Memory: 49.1 MB (15.34%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -3,14 +3,33 @@ class Solution {
         List<Integer>arr=new ArrayList<>();
         int m=nums1.length;
         int n=nums2.length;
-        for(int i=0;i<m;i++){
-            arr.add(nums1[i]);
+        int i=0;
+        int j=0;
+        while(i<m && j<n){
+            if(nums1[i]<nums2[j]){
+                arr.add(nums1[i]);
+                i++;
+            }
+            else if(nums1[i]==nums2[j]){
+                arr.add(nums1[i]);
+                arr.add(nums1[i]);
+                i++;
+                j++;
+            }
+            else{
+                arr.add(nums2[j]);
+                j++;
+            }
         }
-        for(int i=0;i<n;i++){
-            arr.add(nums2[i]);
+        while(i<m){
+            arr.add(nums1[i]);
+            i++;
+        }
+        while(j<n){
+            arr.add(nums2[j]);
+            j++;
         }
         int total=m+n;
-        Collections.sort(arr);
         if(total%2!=0) return arr.get(total/2);
         else return (arr.get(total/2)+arr.get(total/2 -1))/2.0;
     }
