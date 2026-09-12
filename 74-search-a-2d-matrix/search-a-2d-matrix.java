a70031dad6ae0767f32e667ca08f2e197ba358e6
AbhishekVishwakarma-pixel <abhishekvishwakarma200567@gmail.com>
2026-09-12 15:05:28 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (43.42%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,17 +1,17 @@
 class Solution {
     public boolean searchMatrix(int[][] arr, int target) {
-        int m=arr[0].length;
-        int n=arr.length;
-        int low=0;
-        int high=m*n-1;
-        while(low<=high){
-            int mid=low +(high-low)/2;
-            int x=mid/m;
-            int y=mid%m;
-            if(arr[x][y]==target) return true;
-            if(arr[x][y]>target) high=mid-1;
-            else low=mid+1;
-        }
-        return false;
+    int n=arr[0].length;
+    int m=arr.length;
+    int low=0;
+    int high=m*n-1;
+    while(low<=high){
+        int mid=low +(high-low)/2;
+        int x=mid/n;
+        int y=mid%n;
+        if(arr[x][y]==target) return true;
+        else if(arr[x][y]>target) high=mid-1;
+        else low=mid+1;
+    }
+    return false;
     }
 }
