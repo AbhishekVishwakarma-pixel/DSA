57af3cbe5280c44c9ae51f1605c45e00ff2824d7
AbhishekVishwakarma-pixel <abhishekvishwakarma200567@gmail.com>
2026-09-12 13:44:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Row with Max 1s in Rowwise Sorted/row-with-max-1s-in-rowwise-sorted.java
+++ Difficulty: Medium/Row with Max 1s in Rowwise Sorted/row-with-max-1s-in-rowwise-sorted.java
@@ -1,19 +1,18 @@
 class Solution {
     public int rowWithMax1s(int[][] arr) {
         // code here
-        int ind=-1;
         int maxcnt=0;
+        int index=-1;
         for(int i=0;i<arr.length;i++){
             int cnt=0;
-            for(int j=0;j<arr[i].length;j++){
+            for(int j=0;j<arr[0].length;j++){
                 cnt+=arr[i][j];
-            }
-            if(cnt>maxcnt){
-                maxcnt=cnt;
-                ind=i;
+                if(cnt>maxcnt){
+                    maxcnt=cnt;
+                    index=i;
+                }
             }
         }
-        
-        return ind;
+        return index;
     }
 };
