c122edbb5174674d221d8b88b5b8ace03c6ca819
AbhishekVishwakarma-pixel <abhishekvishwakarma200567@gmail.com>
2026-09-13 20:08:37 +0530

    Time: 5 ms (85.20%) | Memory: 44.5 MB (37.33%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i=words.length-1;i>=0;i--){
             if(words[i].length()>0){
                 sb.append(words[i]);
-                sb.append(" ");
+                sb.append(' ');
             }
         }
         return sb.substring(0,sb.length()-1).toString();
