5e8bbe8dcf21c93e61936ae061d63d9c2e3c4be8
AbhishekVishwakarma-pixel <abhishekvishwakarma200567@gmail.com>
2026-09-19 17:35:56 +0530

    Time: 3 ms (76.56%) | Memory: 46.4 MB (61.17%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,4 +1,14 @@
 class Solution {
+    int value(char ch){
+        if(ch=='I') return 1;
+        if(ch=='V') return 5;
+        if(ch=='X') return 10;
+        if(ch=='L') return 50;
+        if(ch=='C') return 100;
+        if(ch=='D') return 500;
+        return 1000;
+
+    }
     public int romanToInt(String s) {
         int result=value(s.charAt(s.length()-1));
         for(int i=s.length()-2;i>=0;i--){
@@ -10,15 +20,5 @@ class Solution {
             }
         }
         return result;
-    }
-    int value(char c){
-        if(c=='I') return 1; 
-        if(c=='V') return 5;
-        if(c=='X') return 10;
-        if(c=='L') return 50; 
-        if(c=='C') return 100; 
-        if(c=='D') return 500;
-
-        return 1000;  
     }
 }
