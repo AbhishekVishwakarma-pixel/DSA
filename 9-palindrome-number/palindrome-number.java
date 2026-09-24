74ca7e01630ba620e4c73ed430941514b232ba64
AbhishekVishwakarma-pixel <abhishekvishwakarma200567@gmail.com>
2026-09-24 19:39:55 +0530

    Time: 6 ms (22.88%) | Memory: 45.9 MB (71.71%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        long num=0;
+        int rem=0;
+        int y=x;
+        while(y>0){
+            rem=y%10;
+            y=y/10;
+            num=num*10+rem;
+        }
+        if(num>Integer.MAX_VALUE || num<Integer.MIN_VALUE) return false;
+        
+        if(num==x) return true;
+        return false;
+    }
+}
