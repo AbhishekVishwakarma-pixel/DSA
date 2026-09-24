c965617752b275cc8c8ec92250858ead7404e193
AbhishekVishwakarma-pixel <abhishekvishwakarma200567@gmail.com>
2026-09-24 18:43:08 +0530

    Time: 14 ms (92.29%) | Memory: 43.2 MB (95.00%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,6 +1,5 @@
 class Solution {
     int expand(String s,int left,int right){
-
         while(left>=0 && right<s.length() && s.charAt(left)==s.charAt(right)){
             left--;
             right++;
